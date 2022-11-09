27b08f919571b5f317e07ac299759c07ca77a4cc
Taha Akhtar(redcrexent) <taha.akhtar.89@gmail.com>
2022-11-09 14:52:21 +0400

    icons style motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,58 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
+import React, { useState } from 'react'
+import HeaderIcon from '../components/HeaderIcon'
+import { motion } from "framer-motion"
+
 type Props = {}
 
 function Header({ }: Props) {
+
+    const [hover, sethover] = useState("gray");
+
+
     return (
         <header className='sticky top-0 flex justify-between max-w-7xl mx-auto p-5 z-20 xl:items-center'>
-            <div className='flex flex-row items-center'>
-                <SocialIcon url='https://www.linkedin.com/in/taha-akhtar-11371629' fgColor='gray' bgColor='transparent' />
-                <SocialIcon url='https://github.com/redcrexent' fgColor='gray' bgColor='transparent' />
-                {/* social Icons */}
-            </div>
-            <div className='flex flex-row items-center text-gray-300 cursor-pointer' >
-                <SocialIcon
-                    fgColor='gray' bgColor='transparent'
-                    network='email'
-                />
-           
-            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
-                let&apos;s connect
-            </p>
-            </div>
+            <motion.div
+                initial={{
+                    x: -500,
+                    scale: 0,
+                    opacity: 0
+                }}
+                animate={{
+                    x: 0,
+                    scale: 1,
+                    opacity: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className='flex flex-row items-center'>
+
+                <HeaderIcon URL='https://www.linkedin.com/in/taha-akhtar-11371629' />
+                <HeaderIcon URL='https://github.com/redcrexent' />
+            </motion.div>
+            <motion.div
+
+                initial={{
+                    x: 500,
+                    scale: 0,
+                    opacity: 0
+                }}
+                animate={{
+                    x: 0,
+                    scale: 1,
+                    opacity: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className='flex flex-row items-center text-gray-300 cursor-pointer' >
+
+                <HeaderIcon Network='email' />
+
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+                    let&apos;s connect
+                </p>
+            </motion.div>
         </header>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
@@ -8,9 +9,16 @@ const Home: NextPage = () => {
       <Head>
         <title>Taha Akhtar&apos;s portfolio</title>
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400&display=swap" rel="stylesheet" />
       </Head>
-      <Header />
-      {/* hero  */}
+      <div className='bg-black text-white h-screen'>
+        <Header />
+
+        <Hero />
+
+      </div>
       {/* about  */}
 
       {/* Experience  */}
